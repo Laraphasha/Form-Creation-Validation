b1ffc94709139c797129261ba295e004c6e4e432
laraphasha <rapelanglara23@gmail.com>
2025-01-31 17:05:46 +0200

    first try

---
+++ script.js
@@ -0,0 +1,37 @@
+document.addEventListener("DOMContentLoaded", (event) => {
+  console.log("DOM loaded ")
+  let form = document.getElementById("registration-form")
+  let feedbackDiv = document.getElementById("form-feedback")
+
+  form.addEventListener("submit", function(e) {
+    event.preventDefault()
+    console.log(document.getElementById("username").value().trim())
+
+    let username = document.getElementById("username").value().trim()
+    let email = document.getElementById("email").value().trim()
+    let password = document.getElementById("password").value().trim()
+
+    isValid = true
+    messages = []
+
+    if (username.length < 3) {
+      isValid = false
+      messages.push("Username is too short")
+    }
+
+    if ((email.includes("@")) && (email.includes("."))) {
+      isValid = false
+      messages.push("Invalid email address")
+    }
+
+    if (password.length < 8) {
+      isValid = false
+      messages.push("Password is too short")
+    }
+
+    if (isValid) {
+      feedbackDiv.innerHTML = "Registration successful!"
+      feedbackDiv.style.color = "#28a745"
+    }
+  })
+})
